c0cb281cb7dc5ca4231cf6ea9ecd0b84d205cbd2
GitEngHar <harukii1999@gmail.com>
2025-04-19 19:13:46 +0900

    delete not need test

--- main_test.go
+++ main_test.go
@@ -110,10 +110,3 @@ func TestFormatSize(t *testing.T) {
 		})
 	}
 }
-
-func TestLogDeletionToFile(t *testing.T) {
-	got := math.Abs(-1)
-	if got != 1 {
-		t.Errorf("Abs(-1) = %f; want 1", got)
-	}
-}
