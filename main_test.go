10c82f617f2099bf7833d56664e93855a1162672
GitEngHar <harukii1999@gmail.com>
2025-04-19 09:44:03 +0900

    TestFormatSizeAdd

--- main_test.go
+++ main_test.go
@@ -27,9 +27,29 @@ func TestToBytes(t *testing.T) {
 }
 
 func TestFormatSize(t *testing.T) {
-	got := math.Abs(-1)
-	if got != 1 {
-		t.Errorf("Abs(-1) = %f; want 1", got)
+	type args struct {
+		bytes int64
+	}
+
+	tests := []struct {
+		name           string
+		args           args
+		wantFormatSize string
+	}{
+		{"MinB", args{0}, "0 B"},
+		{"MaxB", args{1023}, "1023 B"},
+		{"MinKB", args{1024}, "1.00 KB"},
+		{"MaxKB", args{1048575}, "1024.00 KB"},
+		{"MinMB", args{1048576}, "1.00 MB"},
+		{"MaxMB", args{1073741823}, "1024.00 MB"},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotFormatSize := formatSize(tt.args.bytes); gotFormatSize != tt.wantFormatSize {
+				t.Errorf("gotFormatSize = %v\n wantFormatSize = %v", gotFormatSize, tt.wantFormatSize)
+			}
+		})
 	}
 }
 
