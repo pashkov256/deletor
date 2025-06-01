9b5fd8bbd79604742e8418b60f2cf5fc9a0f54a6
pashkov256 <pashkovjob@gmail.com>
2025-06-01 16:24:19 +0300

    bugfix: #175 space menu

--- internal/tui/menu/constants.go
+++ internal/tui/menu/constants.go
@@ -2,8 +2,8 @@ package menu
 
 var (
 	CleanFIlesTitle  = "🧹 Clean files"
-	CleanCacheTitle  = "🗑️  Clean cache"
-	ManageRulesTitle = "⚙️  Manage rules"
+	CleanCacheTitle  = "🗑️ Clean cache"
+	ManageRulesTitle = "⚙️ Manage rules"
 	StatisticsTitle  = "📊 Statistics"
-	ExitTitle        = "🚪  Exit"
+	ExitTitle        = "🚪 Exit"
 )
